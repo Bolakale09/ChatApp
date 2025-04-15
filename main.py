6c548c1c0eb6d02db4b45427fd948aa9ff3984fc
Bolakale09 <Bolakale9797@gmail.com>
2025-04-15 12:46:36 +0100

    Update Uvicorn to bind to 0.0.0.0 for Render deployment

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # This is a sample Python script.
-
+import uvicorn
+from fastapi import FastAPI
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+app = FastAPI()
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,6 +13,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
