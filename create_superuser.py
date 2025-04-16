a2fbeac8c618d60696bfe5f8f1ceb4047dd805a3
Bolakale09 <Bolakale9797@gmail.com>
2025-04-16 16:15:54 +0100

    Updated create_superuser file

--- create_superuser.py
+++ create_superuser.py
@@ -1,9 +1,11 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatapp.settings')  # Replace with your actual settings module
+
 import django
 django.setup()
 
 from django.contrib.auth import get_user_model
 from django.core.management import call_command
-import os
 
 User = get_user_model()
 username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
