c991c5508ccec90a48e948b339974b190afe66bc
Bolakale09 <Bolakale9797@gmail.com>
2025-04-17 13:04:33 +0100

    error fixed

--- chat/urls.py
+++ chat/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.chat_home, name='chat_home'),
     path('signup/', views.signup_view, name='signup'),
     path('chat/<str:username>/', views.chat_room, name='chat_room'),
     path('api/users/', views.user_list, name='user_list'),
