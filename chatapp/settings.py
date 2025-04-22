c1a9524cd2f9331d85785cfaf46a4ddbe73898fe
Bolakale09 <Bolakale9797@gmail.com>
2025-04-22 19:24:24 +0100

    Set LOGIN_URL to /login/

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -186,7 +186,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = 'chat'
 LOGOUT_REDIRECT_URL = 'login'
 
--- chatapp/urls.py
+++ chatapp/urls.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def favicon(request):
+    return HttpResponse(status=204)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('chat.urls')),
-    path('__debug__/', include('debug_toolbar.urls')),  # Add this for Debug Toolbar
+    path('__debug__/', include('debug_toolbar.urls')),
+    path('favicon.ico', favicon),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
