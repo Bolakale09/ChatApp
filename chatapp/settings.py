c009a1f3423f48960bb1695856f6757045f482a6
Bolakale09 <Bolakale9797@gmail.com>
2025-04-26 13:59:56 +0100

    fixed add image error

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DATABASE_URL = config('DATABASE_URL', default='postgres://avnadmin:AVNS_RqbTnyzhEWPm8uiBGTT@chatapp-chat111.d.aivencloud.com:28686/defaultdb?sslmode=require')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['*']
@@ -59,7 +59,7 @@ INSTALLED_APPS = [
 ]
 
 
-if DEBUG:
+if not DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
