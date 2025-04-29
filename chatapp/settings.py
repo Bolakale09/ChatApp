2a4eb4b383d4b74508a9091af52669abe35c29de
Bolakale09 <Bolakale9797@gmail.com>
2025-04-29 13:08:35 +0100

    error updated

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -131,9 +131,17 @@ TEMPLATES = [
 
 # Database
 DATABASE_URL = config('DATABASE_URL', default='postgres://avnadmin:AVNS_RqbTnyzhEWPm8uiBGTT@chatapp-chat111.d.aivencloud.com:28686/defaultdb?sslmode=require')
-DATABASES = {
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / ('db.sqlite3'),
+        }
+    }
+else:
+    DATABASES = {
     'default': dj_database_url.config(default=DATABASE_URL)
-}
+    }
 
 # Redis Configuration
 REDIS_URL = config('REDIS_URL', default="")
